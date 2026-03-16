d2a6803c057cfeb28c12258186b063b3cb4f417f
shiva <shivanishinde082@gmail.com>
2026-03-17 00:45:30 +0530

    update django settings for deployment

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-wn=y0cmu9dury!o@iq*y4)6vtt0_3+8%vw5nuqxk)&cb5ytb7="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
